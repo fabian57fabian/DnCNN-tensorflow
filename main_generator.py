723a7d5735271d6638b142b2b2e78c35057c4067
ScaramuzzinoGiovanna <39622527+ScaramuzzinoGiovanna@users.noreply.github.com>
2020-07-28 10:54:16 +0200

    main of key_generator.py

---
+++ main_generator.py
@@ -0,0 +1,8 @@
+from key_generator import Generator
+import cv2
+
+g = Generator(20, 20)
+trigger_img = g.get_trigger_image()
+cv2.imwrite('img/trigger_image.jpg', trigger_img)
+verification_img = g.get_verification_image(trigger_img)
+cv2.imwrite('img/verification_image.jpg', verification_img)
